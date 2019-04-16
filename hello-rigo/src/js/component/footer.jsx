65bd13fdeea06c9c7035f189f963f26cab619161
Humberto Zarate <zarate.val@gmail.com>
2019-04-16 20:35:49 +0000

    navbar y footer

---
+++ hello-rigo/src/js/component/footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./footer.css";
+
+export class Footer extends React.Component {
+	render() {
+		return (
+			<footer className="py-5 bg-dark">
+				<div className="container">
+					<p className="m-0 text-center text-white">
+						Copyright © Your Website 2019
+					</p>
+				</div>
+			</footer>
+		);
+	}
+}
